50eecef1ce04d8f24ee2e44e9d0ff09999cb752c
Hritick Jaiswal <76635865+hritickjaiswal22@users.noreply.github.com>
2026-06-15 08:04:44 +0530

    Sliding window - Exactly K / at-most-K meta-trick

---
+++ sliding-window/Exactly-K-at-most-K-meta-trick/930.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @param {number} goal
+ * @return {number}
+ */
+var numSubarraysWithSum = function(nums, goal) {
+    const n = nums.length;
+    const postfix = Array(n).fill(-1);
+    let lastOneIndex = -1;
+    let sum = 0;
+    let left = 0;
+    let result = 0;
+
+    for(let i = n - 1;i >= 0;i--) {
+        if(nums[i] === 1) {
+            postfix[i] = i;
+            lastOneIndex = i;
+        } else postfix[i] = lastOneIndex;
+    }
+
+    for(let right=0;right < nums.length;right++) {
+        sum += nums[right];
+
+        while(sum > goal) {
+            sum -= nums[left];
+            left++;
+        }
+
+        if(sum === goal) {
+            if(goal === 0) {
+                result += right - left + 1
+            }
+            else {
+                result += postfix[left] - left + 1
+            }
+        }
+    }
+
+    return result;
+};
